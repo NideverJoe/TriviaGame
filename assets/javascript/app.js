407db6fd1ca64e8d0e03fb2a2ec8a51cbb018865
NideverJoe <nidever.joe@gmail.com>
2019-06-13 13:42:17 -0500

    html wireframe and placeholders are ready. Beginning js.

---
+++ assets/javascript/app.js
@@ -0,0 +1,22 @@
+{/* <script type="text/javascript">
+    $(document).ready(function(){
+        $("input[type='button']").click(function(){
+        	var radioValue = $("input[name='gender']:checked").val();
+            if(radioValue){
+                alert("Your are a - " + radioValue);
+            }
+        });
+        
+    });
+</script>
+</head> 
+<body>
+    <h4>Please select your gender.</h4>
+    <p> 
+        <label><input type="radio" name="gender" value="male">Male</label> 
+        <label><input type="radio" name="gender" value="female">Female</label>
+    </p>
+    <p><input type="button" value="Get Value"></p>
+</body>
+</html>                             */}
+
